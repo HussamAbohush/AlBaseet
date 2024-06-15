882a71a7685fdc14d07418ba00e75a7d26a2d97e
Hussam Aldeen <hussamaldeenabohush@outlook.com>
2024-06-15 11:03:20 +0300

    Adding custom UserControl and finishing up the Loginform

--- AlBaseet.Winform/Forms/LoginForm.cs
+++ AlBaseet.Winform/Forms/LoginForm.cs
@@ -5,17 +5,12 @@ public partial class LoginForm : Form
     public LoginForm()
     {
         InitializeComponent();
+        CenterToScreen();
     }
 
 
     #region FormControls
-    // first load
-    private void LoginForm_Load(object sender, EventArgs e)
-    {
-        pnlLogin.Visible = true;
-        resetPasswordPanel.Visible = false;
-        newPasswordPanel.Visible = false;
-    }
+
     // Moveable without Border
     protected override void WndProc(ref Message m)
     {
@@ -38,48 +33,57 @@ public partial class LoginForm : Form
     // Back button
     private void backButton_Click(object sender, EventArgs e)
     {
-        if (resetPasswordPanel.Visible)
+        if (pnlResetPassword.Visible)
         {
             pnlLogin.Visible = true;
-            resetPasswordPanel.Visible = false;
-            backButton.Visible = false;
+            pnlResetPassword.Visible = false;
+            btnBack.Visible = false;
         }
-        else if (newPasswordPanel.Visible)
+        else if (pnlNewPassword.Visible)
         {
-            newPasswordPanel.Visible = false;
-            resetPasswordPanel.Visible = true;
+            pnlNewPassword.Visible = false;
+            pnlResetPassword.Visible = true;
         }
         else
         {
             pnlLogin.Visible = false;
-            resetPasswordPanel.Visible = true;
+            pnlResetPassword.Visible = true;
 
         }
     }
     #endregion
 
     #region LoginPanel
+    private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+    {
+        if (chkShowPassword.Checked)
+        {
+            txtPassword.showPassword(true);
+        }
+        else
+        {
+            txtPassword.showPassword(false);
+        }
+    }
     private void loginButton_Click(object sender, EventArgs e)
     {
 
     }
-    private void btnResetPassword_Click(object sender, EventArgs e)
-    {
-
-    }
-    private void btnResetPassword2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    private void btnResetPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
     {
         pnlLogin.Visible = false;
-        resetPasswordPanel.Visible = true;
-        backButton.Visible = true;
+        pnlResetPassword.Visible = true;
+        btnBack.Visible = true;
     }
+
     #endregion
 
     #region resetPasswordPanel
     private void resetButton_Click(object sender, EventArgs e)
     {
-        resetPasswordPanel.Visible = false;
-        newPasswordPanel.Visible = true;
+        pnlResetPassword.Visible = false;
+        pnlNewPassword.Visible = true;
+
     }
     #endregion
 
@@ -90,6 +94,8 @@ public partial class LoginForm : Form
     }
 
     #endregion
+
+
 
 
 }
